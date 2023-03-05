fe6dc52f36b4ed2a715c6a806e87f8c03f57967d
Barkın Atıcı <brknatici@hotmail.com>
2023-03-05 21:56:26 +0300

    DAL katmanı yazıldı

--- Project.DAL/ContextClasses/MyContext.cs
+++ Project.DAL/ContextClasses/MyContext.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Project.ENTITIES.Models;
+using Project.MAP.Options;
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.Data.Entity;
@@ -12,13 +14,30 @@ namespace Project.DAL.ContextClasses
     {
         public MyContext() : base("MyConnection")
         {
-
+            
         }
         protected override void OnModelCreating(DbModelBuilder modelBuilder)
         {
 
+            modelBuilder.Configurations.Add(new OrderMap());
+            modelBuilder.Configurations.Add(new CustomerMap());
+            modelBuilder.Configurations.Add(new EmployeeMap());
+            modelBuilder.Configurations.Add(new EmployeeProfileMap());
+            modelBuilder.Configurations.Add(new ExtraMap());
+            modelBuilder.Configurations.Add(new IssueMap());
+            modelBuilder.Configurations.Add(new OrderExtraMap());
+            modelBuilder.Configurations.Add(new SaloonMap());
+            modelBuilder.Configurations.Add(new SaloonIssueMap());
         }
 
-        
+        public DbSet<Order> Orders { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<Employee> Employees { get; set; }
+        public DbSet<OrderExtra> OrderExtras { get; set; }
+        public DbSet<EmployeeProfile> EmployeeProfiles { get; set; }
+        public DbSet<Issue> Issues { get; set; }
+        public DbSet<Saloon> Saloons { get; set; }
+        public DbSet<SaloonIssue> SaloonIssues { get; set; }
+        public DbSet<Extra> Extras { get; set; }
     }
 }
